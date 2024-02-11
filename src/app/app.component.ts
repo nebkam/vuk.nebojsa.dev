c274bc80ae3da8ce481a73317a4e0c5616f0980d
nebojsa.kamber <kamber@nebojsa.dev>
2024-02-11 15:29:37 +0100

    Add toolbar and custom icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon, MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    MatIcon,
+    MatToolbar,
     RouterOutlet,
   ],
   templateUrl: './app.component.html',
@@ -12,4 +17,14 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(
+    private domSanitizer: DomSanitizer,
+    private matIconRegistry: MatIconRegistry
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'wolf',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/wolf.svg')
+    );
+  }
 }
