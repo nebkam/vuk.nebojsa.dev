29df7b05062f4e2390160390847977760d025091
nebojsa.kamber <kamber@nebojsa.dev>
2024-02-11 22:08:31 +0100

    Make a new Sound Toggle component

---
+++ src/app/sound-toggle/sound-toggle.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SoundToggleComponent } from './sound-toggle.component';
+
+describe('SoundToggleComponent', () => {
+  let component: SoundToggleComponent;
+  let fixture: ComponentFixture<SoundToggleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SoundToggleComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SoundToggleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sound-toggle/sound-toggle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sound-toggle',
+  standalone: true,
+  imports: [],
+  templateUrl: './sound-toggle.component.html',
+  styleUrl: './sound-toggle.component.css'
+})
+export class SoundToggleComponent {
+
+}
