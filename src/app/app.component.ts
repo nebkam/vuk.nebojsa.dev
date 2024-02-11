8f171b3c390e4d576e83ea5fe7cc1f008c77f5c9
nebojsa.kamber <kamber@nebojsa.dev>
2024-02-11 22:30:34 +0100

    Navigation tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
 import {MatNavList} from "@angular/material/list";
-import {MatAnchor} from "@angular/material/button";
+import {MatAnchor, MatIconAnchor} from "@angular/material/button";
 import {SoundToggleComponent} from "./sound-toggle/sound-toggle.component";
 import {CustomMatIconRegistryService} from "./custom-mat-icon-registry.service";
 
@@ -14,6 +14,7 @@ import {CustomMatIconRegistryService} from "./custom-mat-icon-registry.service";
   imports: [
     MatAnchor,
     MatIcon,
+    MatIconAnchor,
     MatNavList,
     MatSidenav,
     MatSidenavContainer,
