c0e2029273fe737098d0097d18a36d68908af9ee
nebojsa.kamber <kamber@nebojsa.dev>
2024-02-12 23:16:50 +0100

    Styling

--- src/app/word-spelling/word-spelling.component.ts
+++ src/app/word-spelling/word-spelling.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-word-spelling',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+  ],
   templateUrl: './word-spelling.component.html',
   styleUrl: './word-spelling.component.css'
 })
