22e57e3f40aaf6007861ff3582b9d171650de7be
nebojsa.kamber <kamber@nebojsa.dev>
2024-02-18 12:59:31 +0100

    Add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {GuessTheLetterComponent} from "./guess-the-letter/guess-the-letter.component";
 import {WordSpellingComponent} from "./word-spelling/word-spelling.component";
+import {GuessTheSentenceComponent} from "./guess-the-sentence/guess-the-sentence.component";
 
 export const routes: Routes = [{
   path: 'pogodi-slovo', component: GuessTheLetterComponent
+}, {
+  path: 'pogodi-recenicu', component: GuessTheSentenceComponent
 },{
   path: 'slovo-po-slovo', component: WordSpellingComponent
 }, {
